f72dfd74891ef9d4914fb703b22a83de38f831d3
sukehiro <sukehiro@3films.com>
2019-07-12 02:06:38 +0900

    googleのスニペットを利用して整形

--- app/Http/Controllers/GoogleSheetsController.php
+++ app/Http/Controllers/GoogleSheetsController.php
@@ -8,14 +8,28 @@ use Illuminate\Http\Request;
 
 class GoogleSheetsController extends Controller
 {
-    public function index(GoogleSheets $googleSheets)
-    {
-        $sheets = $googleSheets->instance();
+    protected $googleClient;
 
-        $sheet_id = env('LARAVEL_GOOGLE_SHEETS');
-        $range = 'A1:H1';
-        $response = $sheets->spreadsheets_values->get($sheet_id, $range);
-        $values = $response->getValues();
-        dd($values);
+    public function __construct(GoogleSheets $googleSheets)
+    {
+        $this->googleClient = $googleSheets->instance();
+    }
+
+    public function index()
+    {
+        $spreadsheetId = env('LARAVEL_GOOGLE_SHEETS');
+        $range = 'A:H';
+        dd($this->getValues($spreadsheetId, $range)->getValues());
+    }
+
+    public function getValues($spreadsheetId, $range)
+    {
+        $service = $this->googleClient;
+        // [START sheets_get_values]
+        $result = $service->spreadsheets_values->get($spreadsheetId, $range);
+        $numRows = $result->getValues() != null ? count($result->getValues()) : 0;
+        printf("%d rows retrieved.", $numRows);
+        // [END sheets_get_values]
+        return $result;
     }
 }
